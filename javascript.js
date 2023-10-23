24e2fa825b95ac9fd6822bff408616c4ff279ef5
PekiPho <v.a.r.d.games123@gmail.com>
2023-10-23 23:42:14 +0200

    Added all buttons with basic loop without ==5 wins

--- javascript.js
+++ javascript.js
@@ -1,42 +1,59 @@
- 
-        let p=0;
-        let c=0;
-        function getComputerChoice()
-        {
-            let choicen=Math.floor(Math.random()*3+1);
-            let choice
-            
-            switch(choicen)
-            {
-                case 1: choice="Rock"; break;
-                case 2: choice="Paper"; break;
-                case 3: choice ="Scissors"; break;
-            }
-            return choice;
-        }
-        function playRound(playerSelection, computerSelection)
-        {
-            if(playerSelection.toLowerCase()=== computerSelection.toLowerCase())
-                return "Draw";
-            else if(playerSelection.toLowerCase()==="rock" && computerSelection ==="Scissors")
-                {p++;
-                return "You win! Rock beats Scissors"; }
-            else if(playerSelection.toLowerCase()==="paper" && computerSelection ==="Rock")
-                {p++;
-                return "You win! Paper beats Rock"; }
-            else if(playerSelection.toLowerCase()==="scissors" && computerSelection ==="Paper")
-                {p++;
-                return "You win! Scissors beats Paper";} 
+    let p=0;
+    let c=0;
+    let ps;
+    const buttons = document.querySelectorAll('button');
 
-            else if(playerSelection.toLowerCase()==="rock" && computerSelection ==="Paper")
-                {c++;
-                return "You Lose! Paper beats Rock";}    
-            else if(playerSelection.toLowerCase()==="paper" && computerSelection ==="Scissors")
-                {c++;
-                return "You Lose! Scissors beats Paper";}
-            else if(playerSelection.toLowerCase()==="scissors" && computerSelection ==="Paper")
-                {c++;
-                return "You Lose! Rock beats Scissors"; }
-            else return "Invalid input";
-        }
-        
+    
+    
+    function getComputerChoice()
+    {
+    let choicen=Math.floor(Math.random()*3+1);
+    let choice;
+    switch(choicen)
+    {
+        case 1: choice="rock"; break;
+        case 2: choice="paper"; break;
+        case 3: choice ="scissors"; break;
+    }
+    return choice;
+    }
+
+
+    function playRound(playerSelection, computerSelection)
+    {
+    if(playerSelection=== computerSelection)
+        return "Draw";
+    else if(playerSelection==="rock" && computerSelection ==="scissors")
+        {p++;
+        return "You win! Rock beats Scissors"; }
+    else if(playerSelection==="paper" && computerSelection ==="rock")
+        {p++;
+        return "You win! Paper beats Rock"; }
+    else if(playerSelection==="scissors" && computerSelection ==="paper")
+        {p++;
+        return "You win! Scissors beats Paper";} 
+
+    else if(playerSelection==="rock" && computerSelection ==="paper")
+        {c++;
+        return "You Lose! Paper beats Rock";}    
+    else if(playerSelection==="paper" && computerSelection ==="scissors")
+        {c++;
+        return "You Lose! Scissors beats Paper";}
+    else if(playerSelection==="scissors" && computerSelection ==="rock")
+        {c++;
+        return "You Lose! Rock beats Scissors"; }
+    else return "Invalid input";
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            //console.log(playRound(button.id,getComputerChoice()));
+            const box=document.querySelector('.box');
+            const playa=document.querySelector('.one');
+            const comp=document.querySelector('.two');
+
+            box.textContent=playRound(button.id,getComputerChoice());
+            playa.textContent=p;
+            comp.textContent=c;
+        });
+    });
